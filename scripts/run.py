d4905e2b982edf6a759d00e2f98d0598ff05e13b
Adam Wang <yw287@cornell.edu>
2017-05-19 03:26:57 -0400

    [add] deploy

--- scripts/run.py
+++ scripts/run.py
@@ -5,6 +5,18 @@ app = Flask(__name__, static_url_path='', static_folder='../')
 
 port = os.environ['PORT'] if 'PORT' in os.environ else 5000
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
